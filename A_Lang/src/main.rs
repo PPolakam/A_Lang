4348752e0f27fe65525cf3bdfb4cf9298a6919df
PPolakam <50158288+PPolakam@users.noreply.github.com>
2022-09-30 01:23:12 -0400

    Begun writing the alg to create expr from raw

--- A_Lang/src/main.rs
+++ A_Lang/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 enum OperationType {
     ADDITION,
     SUBTRACTION,
@@ -7,6 +9,19 @@ enum OperationType {
     FIRST,
     SECOND,
     IDLE
+}
+
+impl OperationType {
+    fn to_operation_type(raw: &str) -> OperationType {
+        match raw {
+            "+" => OperationType::ADDITION,
+            "-" => OperationType::SUBTRACTION,
+            "*" => OperationType::MULTIPLICATION,
+            "/" => OperationType::DIVISION,
+            "^" => OperationType::POW,
+            _ => OperationType::IDLE
+        }
+    }
 }
 
 enum ExpressionType {
@@ -21,6 +36,18 @@ struct Expression {
     operation: OperationType
 }
 
+impl ops::Add<Expression> for Expression {
+    type Output = Option<f64>;
+
+    fn add(self, other: Expression) -> Option<f64> {
+        Expression::new(
+            ExpressionType::N(match self.evaluate() { Some(x) => x, None => 0.0 }), // TODO: refactor this code so that it properly handles improper inputs.
+            ExpressionType::N(match other.evaluate() { Some(x) => x, None => 0.0 }),
+            OperationType::ADDITION
+        ).evaluate()
+    }
+}
+
 impl Expression {
 
     fn blank() -> Expression {
@@ -31,12 +58,54 @@ impl Expression {
         }
     }
 
+    fn zero() -> Expression {
+        Expression {
+            arg1: Some(Box::new(ExpressionType::N(0.0))),
+            arg2: None,
+            operation: OperationType::FIRST
+        }
+    }
+
     fn wrap(n: f64) -> Expression {
         Expression {
             arg1: Some(Box::new(ExpressionType::N(n))),
             arg2: None,
             operation: OperationType::FIRST
         }
+    }
+
+    fn new(arg1: ExpressionType, arg2: ExpressionType, op: OperationType) -> Expression {
+        Expression {
+            arg1: Some(Box::new(arg1)),
+            arg2: Some(Box::new(arg2)),
+            operation: op
+        }
+    }
+
+    fn equals(&self, other: Expression) -> bool {
+        false
+    }
+
+    fn to_expression(raw: String) -> Expression {
+
+        let args = raw.split(" ").map(|x| { String::from(x) }).collect::<Vec<String>>(); // use split by scope
+
+        let ops: Vec<(&str, &str)> = vec![("*", "/"), ("+", "-")];
+        let mut acc = Expression::zero();
+
+        for (i, arg) in args.iter().enumerate() {
+            for op in &ops {
+                if arg.eq(op.0) || arg.eq(op.1) {
+                    let arg2 = &args[i + 1];
+
+                    if acc.evaluate().unwrap() == 0.0 {
+
+                    }
+                }
+            }
+        }
+
+        Expression::zero()
     }
 
     fn evaluate(&self) -> Option<f64> {
@@ -96,6 +165,8 @@ fn evaluate(line: &String) -> Expression {
         _ => {}
     };
 
+    let ops: Vec<(&str, &str)> = vec![("*", "/"), ("+", "-")];
+
     let mut tokens: Vec<String> = vec![String::new()];
 
     //&String::from("3 + (x + 5) + ((3x + 2) + 3)")
@@ -104,7 +175,24 @@ fn evaluate(line: &String) -> Expression {
 
     let tokens = tokens.iter().filter(|x| { !(***x).is_empty() }).collect::<Vec<&String>>();
 
-    let tokens = tokens.iter().map(|x| { evaluate(x) });
+    let mut acc = Expression::zero();
+
+    for (i, token) in tokens.iter().enumerate() {
+        for op in &ops {
+            if (*token).eq(op.0) || (*token).eq(op.1) {
+                // 5 + 3 + 8 => (5 + 3) + 8
+                acc = Expression::new(
+                    ExpressionType::Expr(acc),
+                    ExpressionType::Expr(
+                        Expression::zero()
+                    ),
+                    OperationType::FIRST
+                );
+            }
+        }
+    }
+
+
     
     Expression::blank()
 }
